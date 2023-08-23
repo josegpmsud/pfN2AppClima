b1c5d37d5984915e0a2a9faee92cfc7677ec70ff
Jose Gregorio Perez Montilla <solintperez@gmail.com>
2023-08-23 14:00:40 -0400

    creando enlace en la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import './App.css'
 
 function App() {
-  
+  const [weather, setWeather]=useState('');
+  //let latitud = 44.34//7.64
+  //let longitud = 10.99//-72.28
+
+  //Ejemplo Conexion con la app
+
+  //---https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=e920d87ad8a741b2e9c693a7d1e336a7
+
+  //---https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=e920d87ad8a741b2e9c693a7d1e336a7
+
+  useEffect(() => {
+  axios.get('https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=e920d87ad8a741b2e9c693a7d1e336a7') //'https://api.openweathermap.org/data/2.5/forecast?lat='+{latitud}+'&lon='+{longitud}+'&appid=e920d87ad8a741b2e9c693a7d1e336a7'
+  .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response.data);
+    setWeather(response.data);
+  })
+  .catch(function (error) {
+    // manejar error
+    console.log(error);
+  })
+  .finally(function () {
+    // siempre sera executado
+  });
+  }, []);
 
   return (
     <>
       <div>
-        <h1>App Clima</h1>
+      <h1>App Weather</h1>
+      <h4>Ciudad: {JSON.stringify(weather.city.name)}</h4>
+      <h4>Cordenadas: {JSON.stringify(weather.city.coord.lat)} Latitud - {JSON.stringify(weather.city.coord.lon)} longitud</h4>
+      <h4>Zona Horaria: {JSON.stringify(weather.city.timezone)}</h4>
+
+      <h4>Clima:</h4>
+      <h5>Viento: {JSON.stringify(weather.list[0].wind.deg)} deg</h5>
+      <h5>Viento: {JSON.stringify(weather.list[0].wind.gust)} gust</h5>
+      <h5>Viento: {JSON.stringify(weather.list[0].wind.speed)} speed</h5>
       </div>
-      
+
     </>
   )
 }
 
 export default App
+
+/*
+<h1>{JSON.stringify(weather)}</h1>
+*/
+
+/*
+Historia de usuario: Puedo ver el clima de la ciudad como predeterminado, preferiblemente mi ubicación actual
+Historia de usuario: Puedo buscar ciudad
+Historia de usuario: Puedo ver el clima de hoy y los próximos 5 días
+Historia de usuario: Puedo ver la fecha y el lugar del clima
+Historia de usuario: Puedo ver según imagen para cada tipo de clima
+Historia de usuario: Puedo ver el grado mínimo y máximo cada día
+Historia de usuario: Puedo ver el estado y la dirección del viento
+Historia de usuario: Puedo ver el porcentaje de humedad
+Historia de usuario: Puedo ver un indicador de visibilidad
+Historia de usuario: Puedo ver el número de presión de aire
+Historia de usuario (opcional): Puedo solicitar mi ubicación actual meteorológica
+Historia de usuario (opcional): Puedo convertir la temperatura en Celcius a Fahrenheit y viceversa
+*/
