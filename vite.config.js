731b08f4e06b0135a110d8c282e46d814439abfa
josegpmsud <139276330+josegpmsud@users.noreply.github.com>
2023-08-29 08:53:47 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pfN2AppClima/',
+  base: 'https://josegpmsud.github.io/pfN2AppClima/',
   plugins: [react()]
   
 })
